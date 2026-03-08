b51b6067bdf8e7931f00e3534baddb8f48e80385
CoderSohail2003 <163769670+CoderSohail2003@users.noreply.github.com>
2026-03-09 02:24:07 +0530

    Replace 'heyy jenkins' with 'POLL SCM RUNNING...'

--- jenky2.java
+++ jenky2.java
@@ -1,6 +1,6 @@
 public class jenky2 {
     public static void main(String[] args) {
         System.out.println("heyyy jenky");
-        System.out.println("heyy jenkins");
+        System.out.println("POLL SCM RUNNING...");
     }
 }
