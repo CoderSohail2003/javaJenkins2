18bdd0de2223466fbc80871031344effe0d1228f
sohail <sohail@gmail.com>
2026-03-09 23:30:30 +0530

    last trigger

--- jenky2.java
+++ jenky2.java
@@ -3,5 +3,6 @@ public class jenky2 {
         System.out.println("heyyy jenky");
         System.out.println("POLL SCM RUNNING...");
         System.out.println("Triggered using Git-Hub Webhooks");
+        System.out.println("last Trigger");
     }
 }
