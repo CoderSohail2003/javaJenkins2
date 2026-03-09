5849a93b67d7c2832e213d8ffa441dff092e6789
CoderSohail2003 <163769670+CoderSohail2003@users.noreply.github.com>
2026-03-09 21:57:16 +0530

    Add webhook trigger message to jenky2.java

--- jenky2.java
+++ jenky2.java
@@ -2,5 +2,6 @@ public class jenky2 {
     public static void main(String[] args) {
         System.out.println("heyyy jenky");
         System.out.println("POLL SCM RUNNING...");
+        System.out.println("Triggered using Git-Hub Webhooks");
     }
 }
